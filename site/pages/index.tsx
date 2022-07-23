4e5748f65fbcd904521d36b9d4476852758c4fcd
Jaylen A <jasper.avtarovski@education.nsw.gov.au>
2022-07-23 21:35:21 +1000

    something idk

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,28 +1,28 @@
-import commerce from '@lib/api/commerce'
-import { Layout } from '@components/common'
-import { ProductCard } from '@components/product'
-import { Grid, Marquee, Hero } from '@components/ui'
+import commerce from "@lib/api/commerce";
+import { Layout } from "@components/common";
+import { ProductCard } from "@components/product";
+import { Grid, Marquee, Hero } from "@components/ui";
 // import HomeAllProductsGrid from '@components/common/HomeAllProductsGrid'
-import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
+import type { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 
 export async function getStaticProps({
   preview,
   locale,
   locales,
 }: GetStaticPropsContext) {
-  const config = { locale, locales }
+  const config = { locale, locales };
   const productsPromise = commerce.getAllProducts({
     variables: { first: 6 },
     config,
     preview,
     // Saleor provider only
     ...({ featured: true } as any),
-  })
-  const pagesPromise = commerce.getAllPages({ config, preview })
-  const siteInfoPromise = commerce.getSiteInfo({ config, preview })
-  const { products } = await productsPromise
-  const { pages } = await pagesPromise
-  const { categories, brands } = await siteInfoPromise
+  });
+  const pagesPromise = commerce.getAllPages({ config, preview });
+  const siteInfoPromise = commerce.getSiteInfo({ config, preview });
+  const { products } = await productsPromise;
+  const { pages } = await pagesPromise;
+  const { categories, brands } = await siteInfoPromise;
 
   return {
     props: {
@@ -32,7 +32,7 @@ export async function getStaticProps({
       pages,
     },
     revalidate: 60,
-  }
+  };
 }
 
 export default function Home({
@@ -59,7 +59,7 @@ export default function Home({
         ))}
       </Marquee>
       <Hero
-        headline=" Dessert dragée halvah croissant."
+        headline="We are Sulfur."
         description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
       />
       <Grid layout="B" variant="filled">
@@ -85,7 +85,7 @@ export default function Home({
         brands={brands}
       /> */}
     </>
-  )
+  );
 }
 
-Home.Layout = Layout
+Home.Layout = Layout;
